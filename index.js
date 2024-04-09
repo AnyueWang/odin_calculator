a662bb8033820d364641f3632b35300b4e97ea91
AnyueWang <moonwong.way@gmail.com>
2024-04-09 18:29:59 +0200

    Version 0.2: calculation funtioned In this version, you can calculate with all operations. There are still some bugs waiting for fix:     1. The digits of floating numbers should not exceed the maximum limitation;     2. A number divided by 0 should throw an error;     3. A large number with more than 9 digits should throw an error.

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ const btns = document.querySelectorAll(".btn");
 const btnsNum = document.querySelectorAll(".btn-num");
 const btnAc = document.querySelector(".ac");
 const btnDel = document.querySelector(".delete");
+const btnsOperator = document.querySelectorAll(".btn-operator");
+const btnEqual = document.querySelector(".btn-equal");
 const maxDigits = 9;
+const operations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+};
 let numDisplay = document.querySelector(".display-container");
+let currentFormula = [];
+let indicatorNewNum = false;
 
-btns.forEach((btn) => {
-    btn.addEventListener("mouseover", (e) => {
-        e.target.classList.add("btn-hover")
-    });
-    btn.addEventListener("mouseout", (e) => {
-        e.target.classList.remove("btn-hover")
-    });
-});
-
-btnsNum.forEach((btn) => {
+btnsNum.forEach(btn => {
     btn.addEventListener("click", (e) => {
         const numChar = e.target.textContent;
+        if (indicatorNewNum === true) {
+            numDisplay.textContent = "";
+            indicatorNewNum = false;
+            clearActiveOperatorBtn();
+        }
         if (numDisplay.textContent.length < maxDigits) {
-            if (numChar === '.') {
-                if (numDisplay.textContent.includes('.')) { }
+            if (numChar === ".") {
+                if (numDisplay.textContent.includes(".")) { }
                 else { numDisplay.textContent += numChar }
             } else {
                 if (numDisplay.textContent === "0") { numDisplay.textContent = numChar }
@@ -30,10 +36,65 @@ btnsNum.forEach((btn) => {
 });
 
 btnAc.addEventListener("click", () => {
-    numDisplay.textContent = '0';
+    numDisplay.textContent = "0";
+    currentFormula = [];
+    indicatorNewNum = false;
+    btnsOperator.forEach(btn => {
+        if (btn.classList.contains("btn-operator-active")) {
+            btn.classList.remove("btn-operator-active")
+        }
+    })
 });
 
 btnDel.addEventListener("click", () => {
-    if (numDisplay.textContent.length === 1) {numDisplay.textContent = "0"}
-    else {numDisplay.textContent = numDisplay.textContent.slice(0, -1)}
-}); 
+    if (numDisplay.textContent.length === 1) { numDisplay.textContent = "0" }
+    else { numDisplay.textContent = numDisplay.textContent.slice(0, -1) }
+});
+
+btnsOperator.forEach(btn => {
+    btn.addEventListener("click", e => {
+        const operator = e.target.textContent;
+        if (currentFormula.length === 0) {
+            currentFormula.push(numDisplay.textContent);
+            currentFormula.push(operator);
+            e.target.classList.add("btn-operator-active");
+            indicatorNewNum = true;
+        } else {
+            clearActiveOperatorBtn();
+            currentFormula.push(numDisplay.textContent);
+            const result = calculate(currentFormula);
+            numDisplay.textContent = result;
+            currentFormula.pop();
+            currentFormula[0] = result;
+            currentFormula[1] = operator;
+            e.target.classList.add("btn-operator-active");
+            indicatorNewNum = true;
+        }
+    })
+})
+
+btnEqual.addEventListener("click", () => {
+    if (currentFormula.length !== 0) {
+        currentFormula.push(numDisplay.textContent);
+        const result = calculate(currentFormula);
+        numDisplay.textContent = result;
+        currentFormula = [];
+    }
+    clearActiveOperatorBtn();
+    indicatorNewNum = true;
+});
+
+function clearActiveOperatorBtn() {
+    btnsOperator.forEach(btn => {
+        if (btn.classList.contains("btn-operator-active")) {
+            btn.classList.remove("btn-operator-active");
+        }
+    });
+}
+
+function calculate(arr) {
+    const firstNum = arr[0];
+    const operator = arr[1];
+    const secondNum = arr[2];
+    return operations[operator](+firstNum, +secondNum);
+}
